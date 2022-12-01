4a4efa7dcc82972212f7d303be61bedf5c72ca1d
ben <phas0ruk@gmail.com>
2022-12-01 01:07:57 -0800

    completes tutorial

--- client/src/components/ClientRow.js
+++ client/src/components/ClientRow.js
@@ -1,25 +1,23 @@
-import React from "react";
 import { FaTrash } from "react-icons/fa";
 import { useMutation } from "@apollo/client";
 import { DELETE_CLIENT } from "../mutations/clientMutations";
 import { GET_CLIENTS } from "../queries/clientQueries";
+import { GET_PROJECTS } from "../queries/projectQueries";
 
 export default function ClientRow({ client }) {
-  // to have the ID of the client to delete, we can either fetch from the cache, or refetch the data from the server. Both shown below
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    // refetchQueries: [{ query: GET_CLIENTS }], // will refetch the query after the mutation to update the UI. Can be a burden on app performance
-    update(cache, { data: { deleteClient } }) {
-      // updates the cache with the return value from deleteClient
-      // uses caching method here
-      const { clients } = cache.readQuery({ query: GET_CLIENTS });
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter((client) => client.id !== deleteClient.id),
-        },
-      });
-    },
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }], // this refetches the queries after the mutation is done
+    // below is the same as above BUT it uses the cache, faster than refetchQueries
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter((client) => client.id !== deleteClient.id),
+    //     },
+    //   });
+    // },
   });
 
   return (
